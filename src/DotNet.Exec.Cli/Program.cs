9948d9800cd847767fe628326759945fb1a5c21f
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-01 17:52:09 +0100

    Add DotNet.Exec.Cli project

---
+++ src/DotNet.Exec.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet.Exec.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
