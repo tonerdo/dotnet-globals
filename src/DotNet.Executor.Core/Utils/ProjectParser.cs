b4124b8cf1761e1463ecf8f29069d0ada9bdc21f
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-02 22:22:40 +0100

    add ProjectParser utitlity class

---
+++ src/DotNet.Executor.Core/Utils/ProjectParser.cs
@@ -0,0 +1,27 @@
+namespace DotNet.Executor.Core.Utils
+{
+    using Newtonsoft.Json.Linq;
+    using System;
+    using System.IO;
+
+    class ProjectParser
+    {
+        public static string GetPackageName(FileInfo projectJson)
+        {
+            JObject projectObject = JObject.Parse(File.ReadAllText(projectJson.FullName));
+            JToken buildOptions;
+            bool hasBuildOptions = projectObject.TryGetValue("buildOptions", out buildOptions);
+            if (!hasBuildOptions)
+                throw new Exception("Missing buildOptions property in project.json");
+            else
+            {
+                bool hasEntryPoint = buildOptions.Value<bool>("emitEntryPoint");
+                if (!hasEntryPoint)
+                    throw new Exception("Entry point not found in package");
+
+                string outputName = buildOptions.Value<string>("outputName");
+                return outputName ?? projectJson.Directory.Name;
+            }
+        }
+    }
+}
