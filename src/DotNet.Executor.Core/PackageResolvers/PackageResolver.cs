470242cbb600a4975b8e8fe578229618fe81181c
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-02 22:21:26 +0100

    update PackageResolver abstract class

--- src/DotNet.Executor.Core/PackageResolvers/PackageResolver.cs
+++ src/DotNet.Executor.Core/PackageResolvers/PackageResolver.cs
@@ -1,18 +1,33 @@
 namespace DotNet.Executor.Core.PackageResolvers
 {
+    using System;
     using System.IO;
 
     abstract class PackageResolver
     {
         protected DirectoryInfo PackagesFolder { get; set; }
+        protected DirectoryInfo PackageFolder { get; set; }
         protected string Source { get; set; }
 
-        abstract protected Package Resolve();
+        abstract protected void Acquire();
 
         protected PackageResolver(DirectoryInfo packagesFolder, string source)
         {
             this.PackagesFolder = packagesFolder;
             this.Source = source;
+        }
+
+        public Package Resolve()
+        {
+            this.Acquire();
+
+            if (this.PackageFolder == null)
+                throw new Exception("PackageFolder property not set");
+            
+            if (this.PackagesFolder.FullName != this.PackageFolder.Parent.FullName)
+                throw new Exception("Package folder not in specified packages folder");
+
+            return new Package() { Directory = this.PackageFolder, EntryAssemblyFileName = this.PackageFolder.Name + ".dll" };
         }
     }
 }
