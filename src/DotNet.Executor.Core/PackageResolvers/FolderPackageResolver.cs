d411844d269bffcc4219ac6baa873a928c464769
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-02 22:23:57 +0100

    add FolderPackageResolver class

---
+++ src/DotNet.Executor.Core/PackageResolvers/FolderPackageResolver.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Linq;
+using DotNet.Executor.Core.Utils;
+
+namespace DotNet.Executor.Core.PackageResolvers
+{
+    internal class FolderPackageResolver : PackageResolver
+    {
+
+        public FolderPackageResolver(DirectoryInfo packagesFolder, string source) : base(packagesFolder, source) { }
+
+        protected override void Acquire()
+        {
+            var sourceFolder = new DirectoryInfo(this.Source);
+            FileInfo projectJson = sourceFolder.GetFiles().FirstOrDefault(f => f.Name == "project.json");
+
+            if (projectJson == null)
+                throw new Exception("No project.json found in source folder");
+
+            bool restore = ProcessRunner.RunProcess("dotnet", "restore", projectJson.FullName);
+            if (!restore)
+                throw new Exception("Package restore for project failed");
+
+            string packageName = ProjectParser.GetPackageName(projectJson);
+
+            this.PackageFolder = this.PackagesFolder.GetDirectories().FirstOrDefault(d => d.Name == packageName);
+            if (this.PackageFolder != null)
+                this.PackageFolder.Delete();
+
+            this.PackageFolder = this.PackagesFolder.CreateSubdirectory(packageName);
+            bool build = ProcessRunner.RunProcess("dotnet", "build", projectJson.FullName,
+                "-c Release", "-f netcoreapp1.0", "-o " + this.PackageFolder.FullName);
+            if (!build)
+                throw new Exception("Project Compilation failed");
+        }
+    }
+}
