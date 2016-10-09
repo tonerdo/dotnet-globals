e5a48ff1e85d95c40a9f87bcb9919a45e1aa54da
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-09 17:32:46 +0100

    remove empty package husks to avoid package name conflicts

--- src/DotNet.Globals.Core/PackageResolvers/FolderPackageResolver.cs
+++ src/DotNet.Globals.Core/PackageResolvers/FolderPackageResolver.cs
@@ -26,8 +26,10 @@ namespace DotNet.Globals.Core.PackageResolvers
             string packageName = sourceFolder.Name;
 
             this.PackageFolder = this.PackagesFolder.GetDirectories().FirstOrDefault(d => d.Name == packageName);
-            if (this.PackageFolder != null)
+            if (this.PackageFolder != null && this.PackageFolder.GetFiles().Select(f => f.Name).Contains("globals.json"))
                 throw new Exception("A package with the same name already exists");
+            else
+                PackageRemover.RemoveFolder(this.PackageFolder);
 
             this.PackageFolder = this.PackagesFolder.CreateSubdirectory(packageName);
             bool build = ProcessRunner.RunProcess("dotnet", "build", projectJson.FullName,
