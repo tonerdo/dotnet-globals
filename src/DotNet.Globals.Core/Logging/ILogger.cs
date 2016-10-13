6bc3b82e752e7624bc709f96a00df5c08bc5d1ea
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-13 11:26:05 +0100

    add loggin interface

---
+++ src/DotNet.Globals.Core/Logging/ILogger.cs
@@ -0,0 +1,11 @@
+namespace DotNet.Globals.Core.Logging
+{
+    public interface ILogger
+    {
+        void LogSuccess(string data);
+        void LogVerbose(string data);
+        void LogInformation(string data);
+        void LogWarning(string data);
+        void LogError(string data);
+    }
+}
