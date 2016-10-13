1b70e0555dc42e4be4eea8fce21ed8e7ada0b840
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-10-13 23:08:31 +0100

    use reporter logger for nuget logging

--- src/DotNet.Globals.Core/PackageResolvers/NuGetPackageResolver.cs
+++ src/DotNet.Globals.Core/PackageResolvers/NuGetPackageResolver.cs
@@ -116,15 +116,15 @@ namespace DotNet.Globals.Core.PackageResolvers
 
         private class Logger : ILogger
         {
-            public void LogDebug(string data) => Console.WriteLine($"DEBUG: {data}");
-            public void LogVerbose(string data) => Console.WriteLine($"VERBOSE: {data}");
-            public void LogInformation(string data) => Console.WriteLine($"INFORMATION: {data}");
-            public void LogMinimal(string data) => Console.WriteLine($"MINIMAL: {data}");
-            public void LogWarning(string data) => Console.WriteLine($"WARNING: {data}");
-            public void LogError(string data) => Console.WriteLine($"ERROR: {data}");
-            public void LogSummary(string data) => Console.WriteLine($"SUMMARY: {data}");
-            public void LogInformationSummary(string data) => Console.WriteLine($"INFORMATION SUMMARY: {data}");
-            public void LogErrorSummary(string data) => Console.WriteLine($"ERROR SUMMARY: {data}");
+            public void LogDebug(string data) => Reporter.Logger.LogVerbose(data);
+            public void LogVerbose(string data) => Reporter.Logger.LogVerbose(data);
+            public void LogInformation(string data) => Reporter.Logger.LogInformation(data);
+            public void LogMinimal(string data) => Reporter.Logger.LogVerbose(data);
+            public void LogWarning(string data) => Reporter.Logger.LogWarning(data);
+            public void LogError(string data) => Reporter.Logger.LogError(data);
+            public void LogSummary(string data) => Reporter.Logger.LogInformation(data);
+            public void LogInformationSummary(string data) => Reporter.Logger.LogInformation(data);
+            public void LogErrorSummary(string data) => Reporter.Logger.LogError(data);
         }
     }
 }
